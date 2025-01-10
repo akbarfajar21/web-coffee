5b3db97191a6e4b6fcb7ab22d4b4bb1954daad24
akbarfajar21 <akbarfajar2112@gmail.com>
2025-01-10 21:29:35 +0700

    first commit

--- src/Auth/LoginPage.jsx
+++ src/Auth/LoginPage.jsx
@@ -58,58 +58,6 @@ const Login = () => {
     }
   };
 
-  const handleGitHubLogin = async () => {
-    const { user, error } = await supabase.auth.signInWithOAuth({
-      provider: "github",
-    });
-
-    if (error) {
-      Swal.fire({
-        title: "Login Gagal!",
-        text: "Terjadi masalah saat login dengan GitHub.",
-        icon: "error",
-        confirmButtonText: "OK",
-      });
-      return;
-    }
-
-    if (user) {
-      const { data: profileData, error: profileError } = await supabase
-        .from("profiles")
-        .select("id, email, role")
-        .eq("id", user.id)
-        .single();
-
-      if (profileError || !profileData) {
-        const { error: insertError } = await supabase.from("profiles").upsert({
-          id: user.id,
-          username: user.user_metadata.full_name || user.user_metadata.user_name,
-          avatar_url: user.user_metadata.avatar_url,
-          email: user.email,
-          role: "user",
-        });
-
-        if (insertError) {
-          Swal.fire({
-            title: "Error",
-            text: "Gagal menyimpan profil pengguna.",
-            icon: "error",
-            confirmButtonText: "OK",
-          });
-        }
-      }
-
-      Swal.fire({
-        title: "Login Berhasil!",
-        text: "Selamat datang kembali!",
-        icon: "success",
-        confirmButtonText: "OK",
-      }).then(() => {
-        navigate("/");
-      });
-    }
-  };
-
   return (
     <section className="flex items-center justify-center min-h-screen bg-gradient-to-tr from-indigo-700 via-blue-500 to-blue-600 p-6">
       <div className="max-w-4xl w-full bg-white shadow-xl rounded-2xl overflow-hidden flex flex-col md:flex-row">
@@ -145,13 +93,6 @@ const Login = () => {
           >
             <img src="/google-icon.svg" alt="Google" className="w-6 h-6" />
             <span>Login dengan Google</span>
-          </button>
-          <button
-            onClick={handleGitHubLogin}
-            className="w-full bg-gray-500 text-white font-semibold py-3 px-6 rounded-lg shadow-md hover:bg-gray-600 transition-all flex items-center justify-center space-x-3 mt-4"
-          >
-            <img src="/github-icon.png" alt="GitHub" className="w-6 h-6" />
-            <span>Login dengan GitHub</span>
           </button>
         </div>
         <div className="hidden md:block w-1/2 bg-gray-100 p-6 flex justify-center items-center">
